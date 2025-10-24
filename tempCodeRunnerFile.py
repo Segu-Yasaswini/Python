3ef30a44157577d33728575581edaa427a563551
Segu-Yasaswini <seguyasaswini@gmail.com>
2025-10-24 17:44:03 +0530

    updated

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,8 @@
+int = 10
+float = 5
+str = "Yasaswini"
+bool = True
+print(int)
+print(float)
+print(str)
+print(bool)
---
+++ variable.py
@@ -0,0 +1,25 @@
+x = 10
+print(x)
+
+
+name = "yasaswini"
+age = 20
+print("name",name)
+print("age",age)
+
+
+a = b = c = 100
+print(a,b,c)
+
+
+a,b,c = 10,20,30
+print(a,b,c)
+
+int = 10
+float = 5
+str = "Yasaswini"
+bool = True
+print(int)
+print(float)
+print(str)
+print(bool)
