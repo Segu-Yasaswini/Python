2c060f7bbd994605f113f35ba87f51e2ab0757ab
Segu-Yasaswini <seguyasaswini@gmail.com>
2025-10-25 17:50:26 +0530

    updated

---
+++ datatypes.py
@@ -0,0 +1,29 @@
+a = 10
+b = 11.5
+c = True
+d = "python"
+e  = {"name": "Yasaswini"}
+print(type(a),type(b),type(c),type(d))
+
+
+
+print(type(5))
+print(type(6.0))
+
+
+
+print(type(True))
+print(type("True"))
+print(type(1))
+
+
+x = [1,2,3]
+y = (1,2,3)
+z = {1,2,3}
+a = {"name":"Yasaswini"}
+print(type(x), type(y), type(z), type(a))
+
+
+
+val = None
+print(type(val))
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,8 +1,2 @@
-int = 10
-float = 5
-str = "Yasaswini"
-bool = True
-print(int)
-print(float)
-print(str)
-print(bool)
+val = None
+print(type(val))
